1e615c32fb85ac5952f1b72c68ad0d5df7a38870
donggyun112 <ssddgg99@daum.net>
2024-05-02 18:40:01 +0900

    [Server] | modify | f5 새로고침 오류 수정 redirect 문제 수정

--- request/Client.cpp
+++ request/Client.cpp
@@ -227,13 +227,13 @@ Response Client::handleGetRequest(const Config &Conf) {
 	(void)Conf;
     // GET 요청 처리 로직
     const std::string url = this->getUri();
-    std::string filePath = "/Users/seongwol/Documents/github/webserv/html";
+    std::string filePath = "/Users/seodong-gyun/42/webserver/WebServer/html";
     filePath += url;
 
     // std::cout << "filePath: " << filePath << std::endl;
 
     if (url == "/") {
-        filePath += "/index.html";
+        filePath += "index.html";
     }
 
     std::cout << "filePath: " << filePath << std::endl;
