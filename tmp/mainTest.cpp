c87d85f8b3fe1e7b486897bddb97dfe4b5911fb4
박준서 <parkjunseo@bagjunseos-MacBook-Air.local>
2024-05-03 14:54:00 +0900

    [Parse] docs | } 관련된 에러 발견 후 처리

--- tmp/mainTest.cpp
+++ tmp/mainTest.cpp
@@ -1,11 +1,14 @@
 #include "EchoServer.hpp"
 #include "../Parse/Config.hpp"
 
+using namespace std;
+
 int main(int ac, char **av)
 {
 	Config Conf(ac, av);
-	Server Server;
-	Server.makeServerSocket(Conf);
-	Server.queueInit(Conf);
-	Server.run(Conf);
+
+	// Server Server;
+	// Server.makeServerSocket(Conf);
+	// Server.queueInit(Conf);
+	// Server.run(Conf);
 }
