bdb9e4df284e09fdee228c79e0f1777c1101ac82
seongwol <gbssw13@gmail.com>
2024-05-06 22:32:45 +0900

    [project] fix | autoindex 구동 부분 수정

--- request/Client.cpp
+++ request/Client.cpp
@@ -337,10 +337,8 @@ Response Client::handleGetRequest(const Config &Conf) {
         response.setBody(errorBody);
         response.setHeader("Content-Length", std::to_string(errorBody.length()));
         response.setHeader("Connection", "close");
-            std::cout << __LINE__ << std::endl;
         return response;
     }
-            std::cout << __LINE__ << std::endl;
 
     // Location 블록 설정 확인
     LocationConfig loc;
@@ -349,11 +347,9 @@ Response Client::handleGetRequest(const Config &Conf) {
     } catch (const std::exception &e) {
         loc = Conf[_port].getLocation("/");
     }
-            std::cout << __LINE__ << std::endl;
 
     //파일인지, 디렉토리인지, 특수파일인지 확인?
     if (isExtention(HTTP_uri) == true) {
-
         filePath = Server_root + loc.getRoot() + HTTP_uri;
     } else if (isDirectory(Server_root + HTTP_uri) == true) {
 
@@ -361,7 +357,6 @@ Response Client::handleGetRequest(const Config &Conf) {
     } else {
         filePath = Server_root + loc.getRoot() + HTTP_uri.substr(loc.getRoot().length(), HTTP_uri.length() - loc.getRoot().length());
     }
-            std::cout << __LINE__ << filePath <<  std::endl;
 
     // 경로 유효성 검사
     if (!isValidPath(filePath)) {
@@ -372,13 +367,11 @@ Response Client::handleGetRequest(const Config &Conf) {
         response.setBody(errorBody);
         response.setHeader("Content-Length", std::to_string(errorBody.length()));
         response.setHeader("Connection", "close");
-            std::cout << __LINE__ << std::endl;
         return response;
     }
     // HTTP 메서드 허용 검사
     std::vector<std::string> allowMethods = loc.getAllowMethods();
     if (!allowMethods.empty() && std::find(allowMethods.begin(), allowMethods.end(), "GET") == allowMethods.end()) {
-            std::cout << __LINE__ << std::endl;
         return handleMethodNotAllowed();
     }
     // 리다이렉션 처리
@@ -389,7 +382,6 @@ Response Client::handleGetRequest(const Config &Conf) {
         response.setStatusCode(statusCode);
         response.setHeader("Location", returnUrl);
         response.setHeader("Connection", "close");
-            std::cout << __LINE__ << std::endl;
         return response;
     }
 
@@ -403,7 +395,7 @@ Response Client::handleGetRequest(const Config &Conf) {
 
     // 인덱스 파일 설정
     std::string index = loc.getIndex();
-    if (loc.getAutoindex() == false && isDirectory(filePath)) {
+    if (isDirectory(filePath)) {
         if (!index.empty()) {
             filePath += "/" + index;
         }
@@ -431,13 +423,12 @@ Response Client::handleGetRequest(const Config &Conf) {
     //     }
     // }
 
-
     // 파일 확장자 추출
     std::string extension = filePath.substr(filePath.find_last_of(".") + 1);
 
     // 파일 읽기
     std::ifstream file(filePath.c_str(), std::ios::binary);
-    if (isDirectory(filePath) == false && file.is_open()) {
+    if (file.is_open()) {
         // 파일 크기 확인
         file.seekg(0, std::ios::end);
         std::streamsize fileSize = file.tellg();
@@ -453,7 +444,6 @@ Response Client::handleGetRequest(const Config &Conf) {
             response.setBody(errorBody);
             response.setHeader("Content-Length", std::to_string(errorBody.length()));
             response.setHeader("Connection", "close");
-            std::cout << __LINE__ << std::endl;
             return response;
         }
         // 파일 내용 읽기
@@ -466,7 +456,9 @@ Response Client::handleGetRequest(const Config &Conf) {
         response.setHeader("Content-Length", std::to_string(body.length()));
         response.setHeader("Connection", "keep-alive");
         file.close();
-    } else if (isDirectory(filePath) == true && loc.getAutoindex() == true) {
+
+    } else {
+         if (isDirectory(filePath) == true && loc.getAutoindex() == true) {
             std::string autoIndexBody = handleAutoIndex(response, filePath);
             response.setStatusCode(OK_200);
             response.setHeader("Date", getCurTime());
@@ -474,8 +466,6 @@ Response Client::handleGetRequest(const Config &Conf) {
             response.setBody(autoIndexBody);
             response.setHeader("Content-Length", std::to_string(autoIndexBody.length()));
             response.setHeader("Connection", "close");
-            std::cout << __LINE__ << std::endl;
-            return response;
     } else if (isDirectory(filePath) == true && loc.getAutoindex() == false) {
             response.setStatusCode(Forbidden_403);
             response.setHeader("Content-Type", "text/html; charset=utf-8");
@@ -484,9 +474,7 @@ Response Client::handleGetRequest(const Config &Conf) {
             response.setBody(errorBody);
             response.setHeader("Content-Length", std::to_string(errorBody.length()));
             response.setHeader("Connection", "close");
-            std::cout << __LINE__ << std::endl;
-            return response;
-    } else if (isDirectory(filePath) == false) { //이 블록 동작 무조건 안함
+    } else if (isDirectory(filePath) == false && loc.getAutoindex() == false) {
         std::cout << "File not found: " << filePath << std::endl;
         response.setStatusCode(NotFound_404);
         response.setHeader("Content-Type", "text/html; charset=utf-8");
@@ -495,14 +483,20 @@ Response Client::handleGetRequest(const Config &Conf) {
         response.setBody(errorBody);
         response.setHeader("Content-Length", std::to_string(errorBody.length()));
         response.setHeader("Connection", "close");
-        return response;
+    } else if (isDirectory(filePath) == false && loc.getAutoindex() == true) {
+        std::cout << "autoindex and no file" << std::endl;
+        std::string autoIndexBody = handleAutoIndex(response, filePath.substr(0, filePath.find_last_of('/')));
+        response.setStatusCode(OK_200);
+        response.setHeader("Date", getCurTime());
+        response.setHeader("Content-Type", "text/html");
+        response.setBody(autoIndexBody);
+        response.setHeader("Content-Length", std::to_string(autoIndexBody.length()));
+        response.setHeader("Connection", "close");
     }
-
+    }
     response.setHeader("Server", "42Webserv");
-
     return response;
 }
-
 Response handleMethodNotAllowed() {
     Response response;
     response.setStatusCode(MethodNotAllowed_405);
