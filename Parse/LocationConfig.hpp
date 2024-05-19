f8cee0f8be2cec225c7fc21f27416e9bd9723b5b
박준서 <parkjunseo@bagjunseos-MacBook-Air.local>
2024-05-19 14:31:34 +0900

    [Parse] feat | make _etag & isEtag()

--- Parse/LocationConfig.hpp
+++ Parse/LocationConfig.hpp
@@ -31,6 +31,7 @@ class LocationConfig {
     std::vector<int>            _upload_cleanup;
 	std::vector<std::string>    _try_files;
 	bool 					  	_cgi;
+    bool                        _etag;
     
     public:
     ~LocationConfig() {}
@@ -58,6 +59,7 @@ class LocationConfig {
     std::vector<std::string>    getTryFiles() const ;
 	bool						isCgi() const;
 	void						setCgi(bool cgi);
+    bool                        isEtag() const;
     // // 추가적인 필요한 변수들
     std::string                 getAllowedMethod(size_t i) const;
     
